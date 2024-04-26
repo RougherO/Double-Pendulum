5f26e4642a5f935315f59152392398bf4658106a
RougherO <chiragchowdhury51@gmail.com>
2024-04-26 22:07:34 +0530

    Refactored updation calls

--- src/DoublePendulum/DPendulum.cpp
+++ src/DoublePendulum/DPendulum.cpp
@@ -124,75 +124,29 @@ void DPendulum::m_verletSolve(float delta) noexcept
     m_aAcc2       = newAcc2;
 }
 
-std::pair<double, double> DPendulum::fDash(double x1, double x2)
+void DPendulum::m_updatePositions() noexcept
 {
-    // Updating Angular acceleration of each pendulum per frame on this massive formula
-    double num1_1 = -m_gravity * (2 * m_mass1 + m_mass2) * std::sin(m_currAngle1);
-    double num2_1 = m_mass2 * m_gravity * std::sin(m_currAngle1 - 2 * m_currAngle2);
-    double num3_1 = 2 * std::sin(m_currAngle1 - m_currAngle2) * m_mass2 * (x2 * x2 * m_length2 + x1 * x1 * m_length1 * std::cos(m_currAngle1 - m_currAngle2));
-    double den = (2 * m_mass1 + m_mass2 - m_mass2 * std::cos(2 * (m_currAngle1 - m_currAngle2)));
+    // After new angles are calculated, we update the angular
+    // rotation of both pendulums
 
-    double num1_2 = 2 * std::sin(m_currAngle1 - m_currAngle2);
-    double num2_2 = x1 * x1 * m_length1 * (m_mass1 + m_mass2);
-    double num3_2 = m_gravity * (m_mass1 + m_mass2) * std::cos(m_currAngle1);
-    double num4_2 = x2 * x2 * m_length2 * m_mass2 * std::cos(m_currAngle1 - m_currAngle2);
+    m_rod1.setRotation(radToDeg(-m_currAngle1));
+    position_bob1(m_rod1.getPosition());
 
-    return { (num1_1 - num2_1 - num3_1) / (m_length1 * den) - m_dampCoeff1 * m_aVel1, (num1_2 * (num2_2 + num3_2 + num4_2)) / (m_length2 * den) - m_dampCoeff2 * m_aVel2 };
+    position_rod2(m_bob1.getPosition());
+    m_rod2.setRotation(radToDeg(-m_currAngle2));
 }
 
-void DPendulum::RKSolver(double dt)
+void DPendulum::m_updateTrail() noexcept
 {
-    // Updating Angular velocity
-    auto [k1_1, k1_2] = fDash(m_aVel1, m_aVel2); // k1 of bob 1 and 2
-    auto [k2_1, k2_2] = fDash(m_aVel1 + k1_1 * (dt / 2), m_aVel2 + k1_2 * (dt / 2)); // k2 of bob 1 and 2
-    auto [k3_1, k3_2] = fDash(m_aVel1 + k2_1 * (dt / 2), m_aVel2 + k2_2 * (dt / 2));
-    auto [k4_1, k4_2] = fDash(m_aVel1 + k3_1 * (dt), m_aVel2 + k3_2 * (dt));
-
-    m_aVel1 += (dt / 6) * (k1_1 + 2 * (k2_1 + k3_1) + k4_1);
-    m_aVel2 += (dt / 6) * (k1_2 + 2 * (k2_2 + k3_2) + k4_2);
-    m_currAngle1 += m_aVel1 * dt;
-    m_currAngle2 += m_aVel2 * dt;
-
-    // Keeping angles within 2pi not necessary just for fun
-    m_currAngle1 -= static_cast<int>(m_currAngle1 / (std::numbers::pi * 2)) * std::numbers::pi * 2;
-    m_currAngle2 -= static_cast<int>(m_currAngle2 / (std::numbers::pi * 2)) * std::numbers::pi * 2;
-}
-
-void DPendulum::rotate()
-{
-    // Rotating pendulum1 by m_currAngle1 degrees
-    m_line1.setRotation(-(m_currAngle1 * 180) / std::numbers::pi);
-    m_circle1.setPosition(
-        m_line1.getPosition().x + m_length1 * std::sin(m_currAngle1),
-        m_line1.getPosition().y + m_length1 * std::cos(m_currAngle1));
-
-    // Positioning pendulum2 according to pendulum1's rotation
-    m_line2.setPosition(m_circle1.getPosition().x, m_circle1.getPosition().y);
-    m_line2.setRotation(-(m_currAngle2 * 180) / std::numbers::pi);
-    m_circle2.setPosition(
-        m_line2.getPosition().x + m_length2 * std::sin(m_currAngle2),
-        m_line2.getPosition().y + m_length2 * std::cos(m_currAngle2));
-}
-
-void DPendulum::update(double frameTime)
-{
-    while (frameTime > 0) {
-        // Solving DE on every update call to calc next angle of each bob
-        RKSolver(m_dt);
-
-        // Rotating to updated angle
-        rotate();
-        if (trailEnable) {
-            trail.emplace_back(m_circle2.getPosition());
-            if (trail.size() == trailLength + 1) {
-                trail.assign(trail.begin() + 1, trail.end());
-            }
-
-            // Setting an alpha value based on the recency of the point
-            for (auto i = trail.begin(); i < trail.end(); ++i) {
-                i->color = sf::Color(255, 255, 255, (i - trail.begin()) / static_cast<float>(trail.size()) * 100);
-            }
-        }
-        frameTime -= m_dt;
+    for (auto* ptr = trail.begin() + m_trailLength - 1; ptr != trail.begin(); --ptr) {
+        ptr[0].position = ptr[-1].position;
     }
+    trail[0].position = m_bob2.getPosition();
+}
+
+void DPendulum::update(float delta)
+{
+    m_verletSolve(delta);
+    m_updatePositions();
+    m_updateTrail();
 }
