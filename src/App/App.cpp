1893a6e33d0d1818c08de41ac467de4fc0cbb0e2
RougherO <chiragchowdhury51@gmail.com>
2024-05-14 20:44:14 +0530

    App class with ImGui support

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -3,24 +3,34 @@
 #include "imgui-SFML.h"
 
 App::App()
+    : m_window { sf::VideoMode(Defaults::WIN_WIDTH + Defaults::MENU_WIDTH, Defaults::WIN_HEIGHT), "Double Pendulum" }
 {
     m_window.setFramerateLimit(Defaults::FRAME_RATE);
+
     m_pend.position({ Defaults::WIN_WIDTH >> 1, Defaults::WIN_HEIGHT >> 1 });   // set pendulum to middle of window
-    static_cast<void>(ImGui::SFML::Init(m_window));                             // static casting to void of a return value explicitly means
-                                                                                // intentionally ignoring a [[nodiscard]] value
+
+    static_cast<void>(ImGui::SFML::Init(m_window, false));                      // static casting to void means purposefully ignoring a return value marked as [[nodiscard]]
+
+    // Loading fonts
+    // FONT_DIR macro value is passed using CMake at compile time compile_definition
+    // See CMakeList.txt for the actual value
+    ImGui::GetIO().Fonts->AddFontFromFileTTF(FONT_DIR "/arial.ttf", Defaults::FONT_SIZE);
+    static_cast<void>(ImGui::SFML::UpdateFontTexture());
 }
 
 void App::m_handleEvents()
 {
     while (m_window.pollEvent(m_event)) {
+
         ImGui::SFML::ProcessEvent(m_window, m_event);
+
         switch (m_event.type) {
             case sf::Event::Closed:
                 m_window.close();
                 break;
             case sf::Event::KeyPressed:
                 if (m_event.key.code == sf::Keyboard::Space) {
-                    paused = !paused;
+                    m_pend.pause(!m_pend.pause());
                 }
                 break;
             default:
@@ -31,19 +41,41 @@ void App::m_handleEvents()
 
 void App::m_update()
 {
+
     m_pend.update(Defaults::DELTA);
+
     ImGui::SFML::Update(m_window, m_clock.restart());
-    ImGui::ShowDemoWindow();
-    ImGui::Begin("Hello");
-    ImGui::Button("Cute button");
+}
+
+void App::m_draw_ui()
+{
+    ImGui::Begin("Controller", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+
+    // Configuring window
+    ImGui::SetWindowSize({ Defaults::MENU_WIDTH, Defaults::WIN_HEIGHT });
+    ImGui::SetWindowPos({ Defaults::WIN_WIDTH, 0 });
+
+    // Using the loaded font
+    ImGui::PushFont(ImGui::GetIO().Fonts->Fonts[0]);
+
+    ImGui::TextWrapped(m_description.data());
+
+    m_pend.render_imgui();
+
+    ImGui::PopFont();
     ImGui::End();
 }
 
 void App::m_render()
 {
     m_window.clear();
-    ImGui::SFML::Render(m_window);
+
     m_window.draw(m_pend);
+
+    m_draw_ui();
+
+    ImGui::SFML::Render(m_window);
+
     m_window.display();
 }
 
@@ -51,10 +83,8 @@ void App::run()
 {
     while (m_window.isOpen()) {
         m_handleEvents();
-        if (!paused) {
-            m_update();
-            m_render();
-        }
+        m_update();
+        m_render();
     }
 }
 
--- src/App/App.hpp
+++ src/App/App.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include "../DoublePendulum/DPendulum.hpp"
 #include "SFML/Graphics.hpp"
+#include "../DoublePendulum/DPendulum.hpp"
+#include <string_view>
 
 class App {
 public:
@@ -8,21 +9,33 @@ public:
     void run();
     ~App();
 
+    App(App const&)                    = delete;
+    auto operator=(App const&) -> App& = delete;
+    App(App&&)                         = delete;
+    auto operator=(App&&) -> App&      = delete;
+
 private:
     void m_handleEvents();
     void m_update();
+    void m_draw_ui();
     void m_render();
 
     struct Defaults {
         inline uint16_t static constexpr FRAME_RATE { 150 };
         inline uint16_t static constexpr WIN_WIDTH { 800 };
         inline uint16_t static constexpr WIN_HEIGHT { 600 };
+        inline uint16_t static constexpr MENU_WIDTH { 400 };
         inline float static constexpr DELTA { 1 / 15.F };
+        inline float static constexpr FONT_SIZE { 15. };
     };
 
     DPendulum m_pend;
-    sf::RenderWindow m_window { sf::VideoMode(Defaults::WIN_WIDTH, Defaults::WIN_HEIGHT), "Double Pendulum" };
+    sf::RenderWindow m_window;
     sf::Event m_event {};
     sf::Clock m_clock {};
-    bool paused {};
+    std::string_view m_description {
+        "This is an approximate visualization of a double pendulum using modified Velocity Verlet algorithm. All values that cause a state change(marked with *) in the simulation have purposefully"
+        " been made to reset the entire state of the simulation (acceleration, velocity -> 0). This is done so that any change in the state(mass, length etc.) does not"
+        " make the simulation blow up unexpectedly.\n\n* All values in sliders can be manually adjusted by clicking CTRL+LEFT.\n\n* Press Space to pause\n\n"
+    };
 };
